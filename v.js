425ccf0a405fb326271fa22f8840e094f950cb02
ajay <okab2028@gmail.com>
2023-06-13 10:39:55 +0530

    func

--- v.js
+++ v.js
@@ -1,107 +1,107 @@
-// //q1
-// function a()
-// {
-//     console.log("smile")
-// }
-// a();
+//q1
+function a()
+{
+    console.log("smile")
+}
+a();
 
-// // //q2
-// function b()
-// {
-//     var a=3,b=4,total
-//     total=a+b
-//      console.log("sum of",a,",",b,"is",total)
-// }
-// b();
+// //q2
+function b()
+{
+    var a=3,b=4,total
+    total=a+b
+     console.log("sum of",a,",",b,"is",total)
+}
+b();
 
-// //q3
-// const arrow = () => {
-//     console.log("Hello World");
-// }
-// arrow();
+//q3
+const arrow = () => {
+    console.log("Hello World");
+}
+arrow();
 
 //q4
-// var x=21
-// var girl =function()
-// {
-//     console.log(x)
-//     var x=20
-// }
-// girl();
+var x=21
+var girl =function()
+{
+    console.log(x)
+    var x=20
+}
+girl();
 
 
 //q5
-// var x=21
-// girl();
-// console.log(x)
-// function girl()
-// {
-//     console.log(x)
-//     var x=20
-// }
+var x=21
+girl();
+console.log(x)
+function girl()
+{
+    console.log(x)
+    var x=20
+}
 
 
 //q6
 
-// var x=21
-// a()
-// b()
-// function a()
-// {
-//     x=20
-//     console.log(x)
-// }
-// function b()
-// {
-//      x=40
-//     console.log(x)
-// }
+var x=21
+a()
+b()
+function a()
+{
+    x=20
+    console.log(x)
+}
+function b()
+{
+     x=40
+    console.log(x)
+}
 
 //q7
 
-// function fact(n)
-// {
-//      if(n==0)
-//      {
-//         return 1
-//      }
-//      else{
-//         return n* fact(n-1)
-//      }
-// }
-// console.log(fact(5))
+function fact(n)
+{
+     if(n==0)
+     {
+        return 1
+     }
+     else{
+        return n* fact(n-1)
+     }
+}
+console.log(fact(5))
 
 
 
 //q11
 
-// function FindSum(a,b)
-// {
-//     return a+b
-// }
-// function DisplayData(data,batch)
-// {
-//     console.log(`i am from ${data} and My batch is EA${batch} `)
-// }
-// DisplayData("Prepbyte",FindSum(10,9))
+function FindSum(a,b)
+{
+    return a+b
+}
+function DisplayData(data,batch)
+{
+    console.log(`i am from ${data} and My batch is EA${batch} `)
+}
+DisplayData("Prepbyte",FindSum(10,9))
 
 
 //q22
-// Abc()
-// const Abc=function()
-// {
-//     let value=20
-//     console.log(value)
-// }
+Abc()
+const Abc=function()
+{
+    let value=20
+    console.log(value)
+}
 
 //q33
-// var a=10;
-// (function ()
-// {
-//     console.log(a)
-//     var a=20;
-// })
-// ();
+var a=10;
+(function ()
+{
+    console.log(a)
+    var a=20;
+})
+();
 
 //q44
 const greet=function(name)
